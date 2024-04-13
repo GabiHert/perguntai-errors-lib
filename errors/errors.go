a5780f357cc9016dcc8231b6e29a3fcb186c1fbc
GabiHert <hertergabriel4@gmail.com>
2024-04-13 17:25:32 -0300

    first commit

---
+++ errors/errors.go
@@ -0,0 +1,32 @@
+package errors
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ErrorType struct {
+	Code           string `json:"code"`
+	Type           string `json:"type"`
+	HttpStatusCode int    `json:"-"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+	ErrorType
+}
+
+func (e Error) Error() string {
+	return e.Message
+}
+
+func (e Error) ToJSON() ([]byte, error) {
+	return json.Marshal(e)
+}
+
+func (e Error) WriteHttp(writer http.ResponseWriter) (err error) {
+	b, err := e.ToJSON()
+	writer.WriteHeader(e.HttpStatusCode)
+	_, err = writer.Write(b)
+	return
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GabiHert/perguntai-errors-lib
+
+go 1.22.1
